27d6f836eba031445b94bf6582585cfd97ea6cd8
inramkada <121646877+inramkada@users.noreply.github.com>
2026-01-02 07:27:24 +0100

    DSG01 V 0.1

---
+++ site.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Hakyll
+
+main :: IO ()
+main = hakyll $ do
+  -- Static assets (video, images, css, js...)
+  match "assets/**" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  -- Root index (your HTML)
+  match "src/index.html" $ do
+    route $ constRoute "index.html"
+    compile $ getResourceBody
+      >>= relativizeUrls
