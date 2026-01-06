094de713f068a3f856d3ae179805fef358e61e1c
inramkada <121646877+inramkada@users.noreply.github.com>
2026-01-06 20:40:55 +0100

    Delete core directory

--- core/Interop/CKernel.hs
+++
@@ -1,7 +0,0 @@
-module Core.Interop.CKernel where
-
-import GHC.Wasm.Prim (JSVal)
-
-foreign import javascript unsafe
-  "window.ckernel.fast_add($1, $2)"
-  c_fastAdd :: Double -> Double -> IO Double
